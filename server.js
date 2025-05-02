e7094ff47d80f0154dfc34ad9207e0f1e5969032
villanicholas <nicholasvillalobos02@gmail.com>
2025-05-02 10:51:05 -0600

    fixing server

--- server.js
+++ server.js
@@ -308,6 +308,25 @@ router.post('/signin', function (req, res) {
     })
 });
 
+// Authentication endpoints
+router.post('/auth/signup', function(req, res) {
+    if (!req.body.username || !req.body.password) {
+        res.json({success: false, msg: 'Please pass username and password.'});
+    } else {
+        var newUser = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        // save the user
+        newUser.save(function(err) {
+            if (err) {
+                return res.json({success: false, msg: 'Username already exists.'});
+            }
+            res.json({success: true, msg: 'Successful created new user.'});
+        });
+    }
+});
+
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
